f7211d7e5eb00cc752f3a6fff88cd4d6d0137486
RICARDO ZANGELMI FIOROTTO <38324368+ricardozangelmifiorotto@users.noreply.github.com>
2023-01-27 13:00:36 -0300

    Update nota.py

--- nota.py
+++ nota.py
@@ -1,18 +1,26 @@
-# lista de notas
-notas = []
+#lista de média de notas
+medias = []
 
-# adicionar notas
 while True:
-    nota = input("Digite uma nota ou 'q' para sair: ")
-    if nota == 'q':
+    #lista de notas
+    notas = []
+    # Adicionar nome da matéria
+    materia = input("Digite o nome da matéria ou 'q' para sair: ")
+    if materia == 'q':
         break
     else:
-        notas.append(float(nota))
+        # adicionar notas
+        while True:
+            nota = input("Digite uma nota ou 'q' para sair: ")
+            if nota == 'q':
+                break
+            else:
+                notas.append(float(nota))
+        soma = sum(notas)
+        num_notas = len(notas)
+        media = soma / num_notas
+        medias.append((materia,media))
 
-# calcula a média
-soma = sum(notas)
-num_notas = len(notas)
-media = soma / num_notas
-
-# imprime a média
-print("A média de notas é:", media)
+#imprime as médias de cada matéria
+for materia, media in medias:
+    print("A média de notas em", materia, "é:", media)
